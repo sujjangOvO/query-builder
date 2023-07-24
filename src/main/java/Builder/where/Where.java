c35317126c6837cad82fd99807f686ad2f0bae4b
sujjangOvO <pnk7038@gmail.com>
2023-07-24 22:42:22 +0900

    WhereBuilder를 Where의 nested class로 변경

--- src/main/java/Builder/where/Where.java
+++ src/main/java/Builder/where/Where.java
@@ -1,5 +1,6 @@
 package Builder.where;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Where {
@@ -16,5 +17,27 @@ public class Where {
 			str += c.toString();
 		}
 		return str;
+	}
+
+	public static class WhereBuilder {
+		private List<Condition> conditions;
+
+		public WhereBuilder() {
+			this.conditions = new ArrayList<>();
+		}
+
+		public WhereBuilder and(String col, String operation, String value) {
+			conditions.add(new Condition(col, operation, value, "AND"));
+			return this;
+		}
+
+		public WhereBuilder or(String col, String operation, String value) {
+			conditions.add(new Condition(col, operation, value, "OR"));
+			return this;
+		}
+
+		public Where build() {
+			return new Where(conditions);
+		}
 	}
 }
