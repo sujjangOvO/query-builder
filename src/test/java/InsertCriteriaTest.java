99a0a15793af5f851b97fcaecdb2c66dfb5f43dd
sujjangOvO <ssu7038@yu.ac.kr>
2023-08-07 06:00:11 +0900

    :white_check_mark: Insert 빌더 테스트 코드 수정

---
+++ src/test/java/InsertCriteriaTest.java
@@ -0,0 +1,23 @@
+import domain.User;
+import org.junit.jupiter.api.Test;
+
+import builder.Insert;
+
+import java.util.HashMap;
+import java.util.Map;
+
+class InsertCriteriaTest {
+
+	@Test
+	void insertCriteria_test() {
+		Map<String, Object> values = new HashMap<>();
+		values.put("id", 99);
+		values.put("name", "newName");
+		values.put("age", 16);
+
+		Insert insert = Insert.into(User.class)
+						.values(values);
+
+		System.out.println(insert.getQuery());
+	}
+}
