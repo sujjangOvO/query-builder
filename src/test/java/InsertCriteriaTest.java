74dff18aee3c27576783c352250d7f482217c1bf
sujjangOvO <ssu7038@yu.ac.kr>
2023-08-07 22:51:23 +0900

    :white_check_mark: Insert 빌더 테스트 코드 수정

--- src/test/java/InsertCriteriaTest.java
+++ src/test/java/InsertCriteriaTest.java
@@ -8,16 +8,17 @@ import java.util.Map;
 
 class InsertCriteriaTest {
 
-	@Test
-	void insertCriteria_test() {
-		Map<String, Object> values = new HashMap<>();
-		values.put("id", 99);
-		values.put("name", "newName");
-		values.put("age", 16);
+    @Test
+    void insertCriteria_test() {
+        Map<String, Object> values = new HashMap<>();
+        values.put("id", 99);
+        values.put("name", "newName");
+        values.put("age", 16);
 
-		Insert insert = Insert.into(User.class)
-						.values(values);
+        Insert insert = Insert.into(User.class)
+                .values(values)
+                .build();
 
-		System.out.println(insert.getQuery());
-	}
+        System.out.println(insert.getQuery());
+    }
 }
